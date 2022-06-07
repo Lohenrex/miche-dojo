1fe4f7be689ae6ed5839da4641740ddb2ba5c0b2
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-06-07 11:01:54 -0500

    Change the solution to a class and remove magic numbers

--- ip-converter/ip_converter.rb
+++ ip-converter/ip_converter.rb
@@ -1,19 +1,33 @@
-def ipsBetween(start, ending)
-  available_hosts?(diff_ips(start, ending))
-end
+class IpConverter
+	OCTETS = 4.freeze
+	BIT_SIZE = 8.freeze
+	BASE = 2.freeze
+	attr_reader :start, :ending
 
-def diff_ips(start, ending)
-  ip1 = start.split('.')
-  ip2 = ending.split('.')
-  ip3 = []
-  (0...4).each do |i|
-   ip3 << ip2[i].to_i - ip1[i].to_i
-  end
-  ip3.join('.')
-end
+	def initialize(start, ending)
+		@start = start
+		@ending = ending
+	end
 
-def available_hosts?(ip)
-  output = 0
-  ip.split(".").reverse.each_with_index { |byte, i| output += byte.to_i * (256**i)  }
-  output
+	def ipsBetween
+	  available_hosts?(diff_ips)
+	end
+
+	private
+
+	def diff_ips
+	  ip1 = start.split('.')
+	  ip2 = ending.split('.')
+	  ip3 = []
+	  (0...OCTETS).each do |i|
+	   ip3 << ip2[i].to_i - ip1[i].to_i
+	  end
+	  ip3.join('.')
+	end
+
+	def available_hosts?(ip)
+	  output = 0
+	  ip.split(".").reverse.each_with_index { |byte, i| output += byte.to_i * ((BASE ** BIT_SIZE) ** i)  }
+	  output
+	end	
 end
