2f844495b8d5df5015022638c33804e9083bb885
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-06-13 16:30:22 -0500

    Add word_pattern kata

---
+++ word_pattern/word_pattern.rb
@@ -0,0 +1,14 @@
+def word_pattern(pattern, string)
+  resulting_pattern = []
+  word_pattern_map = {}
+  word_array = string.split(' ')
+
+  raise StandardError.new "Pattern and phrase length are not equal" if pattern.length != word_array.length
+
+  word_array.each_with_index do |word, i|
+    word_pattern_map.store(pattern[i], word) if !word_pattern_map.key? pattern[i]
+    resulting_pattern << pattern[i] if word_pattern_map.fetch(pattern[i]) == word
+  end
+
+  pattern == resulting_pattern.join('')
+end
---
+++ word_pattern/word_pattern_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+gem 'minitest', '~> 5.4'
+require 'minitest/autorun'
+require_relative 'word_pattern'
+
+class WordPatternTest < Minitest::Test
+  def test_with_a_one_letter_pattern_and_one_different_word
+    assert_equal word_pattern('aaaa', 'cat cat cat cat'), true
+  end
+  
+  def test_with_a_one_letter_pattern_and_two_different_words
+    assert_equal word_pattern('aaaa', 'cat cat dog cat'), false
+  end
+  
+  def test_with_a_four_different_letter_pattern_and_four_different_words
+    assert_equal word_pattern('abbccd', 'aram zam zam arafi arafi guli'), true
+  end
+  
+  def test_with_a_longer_length_pattern_and_shorter_length_phrase
+    exception = assert_raises(StandardError) {word_pattern('abbcc', 'aram zam zam')}
+    assert_equal("Pattern and phrase length are not equal", exception.message) 
+  end
+end
