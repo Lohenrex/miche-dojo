77e0c8bf4153b4dcfcc329c6415bf5b885cacaf0
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-06-14 15:28:42 -0500

    Refactor kata solution and extract second solution to another file

--- word_pattern/word_pattern.rb
+++ word_pattern/word_pattern.rb
@@ -1,27 +1,20 @@
+# frozen_string_literal: true
+
 def word_pattern(pattern, string)
-  resulting_pattern = []
-  word_pattern_map = {}
-  word_array = string.split(' ')
-
-  raise StandardError.new "Pattern and phrase length are not equal" if pattern.length != word_array.length
-
+  word_array = string.split
+  raise StandardError, 'Pattern and phrase length are not equal' if pattern.length != word_array.length
   return false unless pattern.chars.uniq.length == word_array.uniq.length
 
-  word_array.each_with_index do |word, i|
-    word_pattern_map.store(pattern[i], word) if !word_pattern_map.key? pattern[i]
-    resulting_pattern << pattern[i] if word_pattern_map.fetch(pattern[i]) == word
-  end
-
-  pattern == resulting_pattern.join('')
+  pattern == resulting_pattern(word_array, pattern)
 end
 
-def word_pattern_two_revenge_of_the_zip(pattern, string)
-  word_array = string.split(' ')
-  pattern_chars = pattern.chars
-  raise StandardError.new "Pattern and phrase length are not equal" if pattern_chars.length != word_array.length
+def resulting_pattern(words, pattern)
+  new_pattern = []
+  word_pattern_map = {}
+  words.each_with_index do |word, i|
+    word_pattern_map.store(pattern[i], word) unless word_pattern_map.key? pattern[i]
+    new_pattern << pattern[i] if word_pattern_map.fetch(pattern[i]) == word
+  end
 
-  word_pattern_map = pattern_chars.uniq.zip(word_array.uniq).to_h
-  return false if word_pattern_map.values.any?(&:nil?)
-
-  pattern_chars.zip(word_array).all? { |item| word_pattern_map.fetch(item[0]) == item[1] }
+  new_pattern.join
 end
