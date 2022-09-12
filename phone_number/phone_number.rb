c0c973192b1ac5116cf21a4e5f0686937261567d
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-09-12 16:38:02 -0500

    Allow for phonewords to be parsed

--- phone_number/phone_number.rb
+++ phone_number/phone_number.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 # Formats input into the correct output. In this case, a phone number
-class Formatter
-  def phone_number(numbers)
+class PhoneNumberFormatter
+  LEXICON = [nil,
+             nil,
+             %w[a b c],
+             %w[d e f],
+             %w[g h i],
+             %w[j k l],
+             %w[m n o],
+             %w[p q r s],
+             %w[t u v],
+             %w[w x y z]].freeze
+
+  def format(numbers)
     run_validations(numbers)
+
+    numbers = decode_phonewords(numbers)
 
     "(#{numbers[0..2].join}) #{numbers[3..5].join}-#{numbers[6..9].join}"
   end
@@ -11,8 +24,8 @@ class Formatter
   private
 
   def run_validations(numbers)
-    raise StandardError, 'This is an array exclusive zone. Go away' if input_class_is_invalid?(numbers)
-    raise StandardError, 'Phones only have integers as digits, moron!' if digits_arenot_integers?(numbers)
+    raise StandardError, 'This is an array exclusive zone. KEEP OUT!' if input_class_is_invalid?(numbers)
+    raise StandardError, 'We only accept integers and chars here. Try again!' if digits_are_not_valid?(numbers)
     raise StandardError, 'Hey, we need the 9 digits to create a phone number!' if input_length_is_invalid?(numbers)
     raise StandardError, 'Hey, slow down! just a single digit per array entry' if digit_length_is_invalid?(numbers)
   end
@@ -22,14 +35,20 @@ class Formatter
   end
 
   def digit_length_is_invalid?(numbers)
-    numbers.find { |number| number > 9 }
+    numbers.find { |number| number.is_a?(Integer) && number > 9 }
   end
 
   def input_length_is_invalid?(numbers)
     numbers.size != 10
   end
 
-  def digits_arenot_integers?(numbers)
-    numbers.find { |number| !number.is_a? Integer }
+  def digits_are_not_valid?(numbers)
+    numbers.find { |number| !number.is_a?(Integer) && !number.is_a?(String) }
+  end
+
+  def decode_phonewords(numbers)
+    numbers.map do |number|
+      number.is_a?(Integer) ? number : LEXICON.find_index { |lex| lex&.include? number.downcase }
+    end
   end
 end
--- phone_number/phone_number_test.rb
+++ phone_number/phone_number_test.rb
@@ -7,41 +7,43 @@ require_relative 'phone_number'
 # Test file for the phone_number.rb file
 class PhoneNumberTest < Minitest::Test
   def test_it_works_in_the_happy_path
-    assert_equal(Formatter.new.phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), '(123) 456-7890')
-    assert_equal(Formatter.new.phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), '(111) 111-1111')
+    assert_equal(PhoneNumberFormatter.new.format([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), '(123) 456-7890')
+    assert_equal(PhoneNumberFormatter.new.format([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), '(111) 111-1111')
+  end
+
+  def test_it_works_with_words_in_the_happy_path
+    assert_equal(PhoneNumberFormatter.new.format([8, 0, 0, 'L', 'L', 'A', 'M', 'A', 'Y', 'A']), '(800) 552-6292')
+    assert_equal(PhoneNumberFormatter.new.format([6, 6, 'A', 'Y', 'm', 'i', 'g', 'u', 'e', 'l']), '(662) 964-4835')
   end
 
   def test_it_fails_when_not_using_arrays
-    exception = assert_raises(StandardError) { Formatter.new.phone_number(1) }
-    assert_equal('This is an array exclusive zone. Go away', exception.message)
+    exception = assert_raises(StandardError) { PhoneNumberFormatter.new.format(1) }
+    assert_equal('This is an array exclusive zone. KEEP OUT!', exception.message)
 
-    exception = assert_raises(StandardError) { Formatter.new.phone_number({}) }
-    assert_equal('This is an array exclusive zone. Go away', exception.message)
+    exception = assert_raises(StandardError) { PhoneNumberFormatter.new.format({}) }
+    assert_equal('This is an array exclusive zone. KEEP OUT!', exception.message)
   end
 
   def test_it_fails_when_not_single_digits
-    exception = assert_raises(StandardError) { Formatter.new.phone_number([14, 2, 553, 46, 5, 76, 7, 8, 954, 0]) }
+    exception = assert_raises(StandardError) { PhoneNumberFormatter.new.format([14, 2, 553, 46, 5, 76, 7, 8, 954, 0]) }
     assert_equal('Hey, slow down! just a single digit per array entry', exception.message)
 
     exception = assert_raises(StandardError) do
-      Formatter.new.phone_number([1, 1, 167, 1565, 178, 155, 1, 71, 15_556, 1])
+      PhoneNumberFormatter.new.format([1, 1, 167, 1565, 178, 155, 1, 71, 15_556, 1])
     end
     assert_equal('Hey, slow down! just a single digit per array entry', exception.message)
   end
 
   def test_it_fails_when_not_enough_phone_digits
-    exception = assert_raises(StandardError) { Formatter.new.phone_number([]) }
+    exception = assert_raises(StandardError) { PhoneNumberFormatter.new.format([]) }
     assert_equal('Hey, we need the 9 digits to create a phone number!', exception.message)
 
-    exception = assert_raises(StandardError) { Formatter.new.phone_number([1, 2, 3, 4, 5, 6, 7, 8]) }
+    exception = assert_raises(StandardError) { PhoneNumberFormatter.new.format([1, 2, 3, 4, 5, 6, 7, 8]) }
     assert_equal('Hey, we need the 9 digits to create a phone number!', exception.message)
   end
 
   def test_it_fails_when_not_using_integers_as_digits
-    exception = assert_raises(StandardError) { Formatter.new.phone_number([1, '@', 'a33er', 2, 2, 2, 2, 2]) }
-    assert_equal('Phones only have integers as digits, moron!', exception.message)
-
-    exception = assert_raises(StandardError) { Formatter.new.phone_number([5, 4, 4.3, 2, 1, 1, 1, 1, 1]) }
-    assert_equal('Phones only have integers as digits, moron!', exception.message)
+    exception = assert_raises(StandardError) { PhoneNumberFormatter.new.format([5, 4, 4.3, 2, 1, 1, 1, 1, 1]) }
+    assert_equal('We only accept integers and chars here. Try again!', exception.message)
   end
 end
